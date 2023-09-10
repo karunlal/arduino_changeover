29c89181a5607ef4364ddad2c33caf079c15bd6f
karunlal <karunlalp@gmail.com>
2023-09-10 14:08:41 +0530

    millis() added for 10 seconds before switching to relay a and changed initial to  state of relay b to low

--- changeover.ino
+++ changeover.ino
@@ -1,32 +1,36 @@
-const int relayPinA = 2;         // Digital output pin for relay
+const int relayPinA = 2; // Digital output pin for relay
 const int relayPinB = 3;
 
 double sensorValue1 = 0;
 int val[100];
 int max_v = 0;
 double Veff = 0;
+unsigned long startTime = 0; // Declaration of startTime variable
 
 void setup() {
   Serial.begin(9600);
   pinMode(relayPinA, OUTPUT);
   pinMode(relayPinB, OUTPUT);
-  digitalWrite(relayPinA, HIGH); // Initialize relays in OFF state
-  digitalWrite(relayPinB, HIGH);
+  digitalWrite(relayPinA, HIGH); // Initialize relays in the OFF state
+  digitalWrite(relayPinB, LOW);
 }
 
 void loop() {
   float volt = voltage();
   if (volt > 200) {
-    digitalWrite(relayPinA, LOW); // Turn ON relay A
-    digitalWrite(relayPinB, HIGH);  // Turn OFF relay B
-    Serial.println("Activating Relay A");
+    if (millis() - startTime >= 10000) { // Check if 10 seconds have elapsed
+      digitalWrite(relayPinA, LOW); // Turn ON relay A
+      digitalWrite(relayPinB, HIGH); // Turn OFF relay B
+      Serial.println("Activating Relay A");
+    }
   } else {
-    
-    
     digitalWrite(relayPinB, LOW); // Turn ON relay B
-    digitalWrite(relayPinA, HIGH);  // Turn OFF relay A
+    digitalWrite(relayPinA, HIGH); // Turn OFF relay A
     Serial.println("Activating Relay B");
+    startTime = millis(); // Reset the start time when voltage drops below 200
   }
+
+  delay(1000); // Add a delay to control the measurement rate
 }
 
 float voltage() {
@@ -58,6 +62,4 @@ float voltage() {
   Serial.print("Voltage: ");
   Serial.println(Veff);
   return Veff;
-  delay (1000);
-
 }
